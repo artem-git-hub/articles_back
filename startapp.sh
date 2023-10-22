d9dcab58a3013ca99a1885118b0f280f29a3f764
artem <arehov888@gmail.com>
2023-10-22 10:47:36 +0300

    add requiremets for env

--- startapp.sh
+++ startapp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/www/articles/back/venv/bin/activate
+source /home/www/articles/back_venv/bin/activate
 cd /home/www/articles/back/
 uvicorn server:app --host 0.0.0.0 --port 8888 --reload
